2971bddb1b6b7d99142445256c9be26833fd4364
Jhon Esteban Palacio <jesteban.palacio@udea.edu.co>
2025-10-04 16:38:25 -0500

    Initial version of labo1 generated by generator-jhipster@8.11.0

---
+++ src/main/webapp/app/config/dayjs.ts
@@ -0,0 +1,19 @@
+import dayjs from 'dayjs';
+import customParseFormat from 'dayjs/plugin/customParseFormat';
+import duration from 'dayjs/plugin/duration';
+import relativeTime from 'dayjs/plugin/relativeTime';
+
+// jhipster-needle-i18n-language-dayjs-imports - JHipster will import languages from dayjs here
+import 'dayjs/locale/es';
+import 'dayjs/locale/zh-cn';
+import 'dayjs/locale/zh-tw';
+import 'dayjs/locale/en';
+import 'dayjs/locale/fr';
+import 'dayjs/locale/de';
+import 'dayjs/locale/it';
+import 'dayjs/locale/ja';
+
+// DAYJS CONFIGURATION
+dayjs.extend(customParseFormat);
+dayjs.extend(duration);
+dayjs.extend(relativeTime);
